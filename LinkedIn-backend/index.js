01e686f4a5046bda3f548e2e088a88fe46f934f2
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 17:55:54 +0300

    Backend: Creating my server and connecting to database

---
+++ LinkedIn-backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+require('./config/db.config');
+require('dotenv').config();
+
+
+
+app.listen(process.env.PORT, (err)=>{
+    if(err) throw err;
+    console.log(`server running on port ${process.env.PORT}`);
+})
