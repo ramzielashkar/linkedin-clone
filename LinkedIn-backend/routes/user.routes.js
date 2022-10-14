0c920cb539d0a7f7f20c389fb0391a7af0d5e5df
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 22:38:22 +0300

    Backend: Updating user routes

--- LinkedIn-backend/routes/user.routes.js
+++ LinkedIn-backend/routes/user.routes.js
@@ -1,11 +1,11 @@
 const {Router} = require('express');
-const { applyToJob } = require('../controllers/user.controller');
+const { applyToJob, followCompany } = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const userMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
 router.post('/apply/', authMiddleware, userMiddleware, applyToJob);
-
+router.get('/follow/:name', authMiddleware, userMiddleware, followCompany);
 
 
 
