0e02baf899b4c6245ad2cf28de24babb0e14a6a6
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 18:35:08 +0300

    Backend: Creating Job model

---
+++ LinkedIn-backend/models/jobs.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const {Application, applicationSchema} = require('./application.model');
+const User = require('./users.model');
+
+// Creating User Schema
+const jobSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'name is required'
+    },
+    description:{
+        type: String,
+        required: 'Description is required'
+    },
+    type:{
+        type: String,
+        required: 'Type is required'
+    },
+    company_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    applications : [applicationSchema]
+})
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
