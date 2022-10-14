4cf5dc2845ddc1cf6637810905f92dedf9dcca75
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 18:34:27 +0300

    Backend: Creating Application model

---
+++ LinkedIn-backend/models/application.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const User = require('./users.model');
+
+// Creating User Schema
+const applicationSchema = new mongoose.Schema({
+    User_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    details: {
+        type: String,
+        required : 'details are required'
+    },
+
+})
+
+const Application = mongoose.model('Application', applicationSchema);
+
+module.exports = {
+    Application,
+    applicationSchema    
+};
