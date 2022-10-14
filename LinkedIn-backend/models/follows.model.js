0b692e4ada734bb676ac5ffdfda7fb13106f90ad
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 20:22:08 +0300

    Backend: Creating Follow model

---
+++ LinkedIn-backend/models/follows.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const {Application, applicationSchema} = require('./application.model');
+const User = require('./users.model');
+
+// Creating User Schema
+const followSchema = new mongoose.Schema({
+
+    user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    company_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+})
+
+const Follow = mongoose.model('Follow', followSchema);
+
+module.exports = Job;
