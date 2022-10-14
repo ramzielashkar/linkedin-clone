922f3104e46b8c8396506fae047d7ecb30130bec
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 18:02:15 +0300

    Backend: Creating User model

---
+++ LinkedIn-backend/models/users.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// Creating User Schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'name is required'
+    },
+    email: {
+        type: String,
+        required: 'email is required',
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: 'password is required',
+        select: false
+    },
+    type :{
+        type: String,
+        required : 'user type is required' 
+    },
+    bio:{
+        type: String,
+    },
+    profile_picture:{
+        type: String
+    },
+
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
