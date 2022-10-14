eec4f2ec318dcbbb612a8b6ada00b7b46a6726f8
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 22:11:31 +0300

    Backend: Adding User middleware

---
+++ LinkedIn-backend/middlewares/user.middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/users.model');
+
+// Authentication middleware
+const userMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await User.findOne({email: decoded.email}).lean();
+        if(user.type != 'user') return res.status(401).json({message: "Unauthorized"});
+        next()
+
+    }catch(err){
+        return res.status(401).json({message: "Unauthorized"})
+    }
+
+}
+
+module.exports = userMiddleware;
+
