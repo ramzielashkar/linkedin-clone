87efe76c3a917f681dd26b598541d1e686260af8
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 22:11:48 +0300

    Backend: Including User middleware

--- LinkedIn-backend/routes/user.routes.js
+++ LinkedIn-backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 const { applyToJob } = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+const userMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
-router.post('/apply/', authMiddleware, applyToJob);
+router.post('/apply/', authMiddleware, userMiddleware, applyToJob);
 
 
 
