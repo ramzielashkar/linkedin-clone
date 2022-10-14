ed18996c8f607a8e66d3961013c798eb5ebfa472
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 21:01:33 +0300

    Backend: Adding Company routes

--- LinkedIn-backend/index.js
+++ LinkedIn-backend/index.js
@@ -3,9 +3,12 @@ const app = express();
 app.use(express.json());
 require('./config/db.config');
 require('dotenv').config();
+
 const authRoutes = require('./routes/auth.route');
 app.use('/auth', authRoutes)
 
+const companyRoutes = require('./routes/company.routes');
+app.use('/jobs', companyRoutes)
 
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
