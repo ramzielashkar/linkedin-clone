dc27cc1151f167cec9d8195a0212d64720e1b3dd
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 21:23:01 +0300

    Backend: Including company middleware

--- LinkedIn-backend/routes/company.routes.js
+++ LinkedIn-backend/routes/company.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const { addJob, getJobs } = require('../controllers/company.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+const companyMiddleware = require('../middlewares/company.middleware');
 const router = Router();
 
-router.post('/', authMiddleware, addJob);
-router.get('/', authMiddleware, getJobs);
+router.post('/', authMiddleware, companyMiddleware, addJob);
+router.get('/', authMiddleware, companyMiddleware, getJobs);
 
 
 module.exports = router;
