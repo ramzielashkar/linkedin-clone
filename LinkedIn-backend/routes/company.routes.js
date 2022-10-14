8bd9c546fb2a7a1b7c123f3448035eda07ea4983
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-14 21:11:10 +0300

    Backend: Adding Get jobs route

--- LinkedIn-backend/routes/company.routes.js
+++ LinkedIn-backend/routes/company.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const { addJob } = require('../controllers/company.controller');
+const { addJob, getJobs } = require('../controllers/company.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = Router();
 
 router.post('/', authMiddleware, addJob);
+router.get('/', authMiddleware, getJobs);
 
 
 module.exports = router;
