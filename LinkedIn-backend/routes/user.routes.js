88e762c7a6e43e48be2c7582fcb0b24c8b38adb2
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-15 11:39:06 +0300

    Backend: Adding search route

--- LinkedIn-backend/routes/user.routes.js
+++ LinkedIn-backend/routes/user.routes.js
@@ -1,12 +1,11 @@
 const {Router} = require('express');
-const { applyToJob, followCompany } = require('../controllers/user.controller');
+const { applyToJob, followCompany, search } = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const userMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
 router.post('/apply/', authMiddleware, userMiddleware, applyToJob);
 router.get('/follow/:name', authMiddleware, userMiddleware, followCompany);
-
-
+router.get('/search/:job', authMiddleware, userMiddleware, search);
 
 module.exports = router;
