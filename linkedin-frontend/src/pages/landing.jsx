1401d2b1a13759e2347ccbda058a7ab04a222651
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-15 20:27:13 +0300

    Frontend: Creating Landing Page

---
+++ linkedin-frontend/src/pages/landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LandingNav from '../components/LandingNav';
+
+const Landing = (props) => {
+  return (
+    <>
+        <LandingNav/>
+    </>
+    )
+}
+
+
+export default Landing;
