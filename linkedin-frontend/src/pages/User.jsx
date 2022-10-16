43ff19cbcdb2e97e5b1167641907f1126589c9c1
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-16 13:54:48 +0300

    Frontend: Creating User Page

---
+++ linkedin-frontend/src/pages/User.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import LandingNav from "../components/LandingNav";
+
+const User = () =>{
+    return (
+        <>
+            <LandingNav 
+            path={'feed'}/>
+            <Outlet/>
+        </>
+    );
+};
+
+export default User;
