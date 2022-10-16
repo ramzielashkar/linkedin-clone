273fa905e11c2de9abc68dbc834d37f743323e54
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-16 12:50:36 +0300

    Frontend: Updating input component

--- linkedin-frontend/src/components/Input.jsx
+++ linkedin-frontend/src/components/Input.jsx
@@ -1,9 +1,17 @@
-const Input = ({type, placeholder, onChange}) => {
+const Input = ({type, placeholder, onChange, path}) => {
+    if(path=='sigin'){
     return(
         <>
             <input className="px-3 rounded-sm py-3 border w-4/5 border-black border-opacity-90 text-lg text-black mb-4" type={type} placeholder={placeholder} onChange = {onChange}></input>
         </>
     );
+    }else{
+        return(
+            <>
+                <input className="px-3 rounded-sm py-3 border w-full border-black border-opacity-90 text-lg text-black mb-4" type={type} placeholder={placeholder} onChange = {onChange}></input>
+            </>
+        );
+    }
 }
 
 export default Input
