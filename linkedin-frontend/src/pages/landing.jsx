e754d5732a6056853b4aff0cb88bcd016eac6359
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-16 12:16:25 +0300

    Frontend: Updating Signup form

--- linkedin-frontend/src/pages/landing.jsx
+++ linkedin-frontend/src/pages/landing.jsx
@@ -27,7 +27,12 @@ const Landing = () => {
                     LinkedIn <a className='text-primary font-bold' href=''>User Agreement</a>, <a className='text-primary font-bold' href=''>Privacy Policy</a>,
                      and <a className='text-primary font-bold' href=''>Cookie Policy</a>.</p>
                      <input type="submit" value = "Agree & Join" className='w-4/5 py-4 text-xl text-center bg-primary rounded-full opacity-80 hover:opacity-100 text-white cursor-pointer mb-4'/>
-                     
+                     <div className='flex w-4/5 items-center mb-6'>
+                        <div className='w-48 border-gray-400 border bg-gray-400'></div>
+                        <p className='mx-6 text-gray-500 text-lg text-bold'>or</p>
+                        <div className='w-48 border-gray-400 border bg-gray-400'></div>
+                     </div>
+                     <p className='w-4/5 text-gray-600 text-lg font-normal text-center'>Already on LinkedIn? <a className='font-bold cursor-pointer text-purple-600 hover:underline'>Sign in</a></p>
                 </form>
             </div>
         </div>
