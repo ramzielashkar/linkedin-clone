3cffcfcfeaf695f1276ac991bb0f2b0e5995f608
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-16 10:55:18 +0300

    Frontend: Updating  landing navbar

--- linkedin-frontend/src/components/LandingNav.jsx
+++ linkedin-frontend/src/components/LandingNav.jsx
@@ -1,5 +1,8 @@
 import logo from '../assets/logo.svg';
-const LandingNav = ()=>{
+const LandingNav = ({path})=>{
+    if(path='login'){
+
+    
     return(
         <nav className= "flex px-56 py-6 items-center">
             <div className="font-sans font-bold text-4xl text-primary mr-1">Linked</div>
@@ -8,6 +11,7 @@ const LandingNav = ()=>{
             </div>
         </nav>
     );
+    }
 }
 
 export default LandingNav;
