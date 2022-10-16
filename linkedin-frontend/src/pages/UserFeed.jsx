59e8160e1c3fa98afd7caf5b9cee0a7e7384ccda
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-16 15:08:21 +0300

    Frontend: Creating left bar

--- linkedin-frontend/src/pages/UserFeed.jsx
+++ linkedin-frontend/src/pages/UserFeed.jsx
@@ -1,8 +1,28 @@
+import cover from '../assets/cover.PNG'
+import profile from '../assets/profile.jpg'
 const UserFeed = () => {
     return (
-        <>
-            <h1>hello</h1>
-        </>
+        <section className="bg-background flex px-56 py-4">
+            <div className="flex flex-col bg-white rounded-xl pb-4 w-1/5 mr-8">
+                <div className=" relative flex rounded-xl h-18">
+                    <img className='rounded-t-xl' src={cover} alt=""/>
+                    <div className="absolute rounded-full border left-24 top-7">
+                        <img className='rounded-full' src={profile} alt="" width={60} height ={60} />
+                    </div>
+                </div>
+                <div className='bg-white flex flex-col items-center justify-center mt-10'>
+                    <a className='font-semibold mb-2 hover:underline' href="">Ramzi El Ashkar</a>
+                    <p className='opacity-80 text-sm'>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="bg-white rounded-xl flex flex-col py-6 px-4">
+                <p className='text-xl font-semibold '>Jobs For You</p>
+                <p className='opacity-80 text-base mb-10'>Because you expressed interest in remote work</p>
+
+                <div></div>
+            </div>
+        </section>
     );
 };
 
