51461a6d8cfac4fff28483de408f78f663f3824a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-16 20:13:14 +0300

    Frontend: Creating Profile Page

---
+++ linkedin-frontend/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import LandingNav from "../components/LandingNav";
+const Profile = () => {
+    return(
+        <>
+         <LandingNav 
+            path={'profile'}/>
+        </>
+    );
+
+}
+
+export default Profile;
