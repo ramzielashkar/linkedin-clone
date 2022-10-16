786c69baaaa3faff8fab4e5b7e4fc579e987d521
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-16 21:39:16 +0300

    Frontend: Configuring colors used

--- linkedin-frontend/tailwind.config.js
+++ linkedin-frontend/tailwind.config.js
@@ -6,10 +6,19 @@ module.exports = {
       animation: {
         "spin-slow": "spin 3s linear infinite",
       },
+      colors: {
+        primary: '#0a66c2',
+        landing: '#8F5849',
+        secondary:'RGB(238, 243, 248)',
+        background : '#f3f2ef',
+        popup : 'rgba(0,0,0,0.5)',
+        blackpopup : '#1d2226'
+      },
+      spacing: {
+        '128': '38rem',
+      }
     },
-    colors: {
-      primary: '#0a66c2',
-    }
+
   },
   plugins: [],
 };
