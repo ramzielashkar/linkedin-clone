fff3e54d4d05b083e7907887fbb39eed70227584
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-18 13:44:05 +0300

    Frontend: Creating signup hook

---
+++ linkedin-frontend/src/query/auth/authorization.js
@@ -0,0 +1,8 @@
+import { axiosInstance } from "../axios";
+import {queryClient} from "../../App";
+import { useQuery, useMutation} from "@tanstack/react-query";
+
+export const registerUser = async (data) => {
+    return await axiosInstance.post("auth/signup", data);
+}
+
