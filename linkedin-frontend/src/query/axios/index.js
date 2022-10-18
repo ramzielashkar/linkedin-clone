36ecba0083601c144371ba04e4f78c222dd82b5b
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-18 14:07:06 +0300

    Frontend: Updating axios instance

--- linkedin-frontend/src/query/axios/index.js
+++ linkedin-frontend/src/query/axios/index.js
@@ -5,12 +5,10 @@ export const axiosInstance = axios.create(
         baseURL: "http://127.0.0.1:8000",
         headers:{
             'Authorization': `Bearer ${token}`,
-            "Content-type": "application/json",
             "Access-Control-Allow-Origin": "*",
             'Access-Control-Allow-Headers': "*",
             'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
             'Access-Control-Allow-Credentials': true
-
         }
     }
 )
